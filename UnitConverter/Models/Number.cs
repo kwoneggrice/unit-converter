d24c59f5072d9175feb49a7dda70ffce27d78e67
jeongin <kwjddls1919@gmail.com>
2023-09-06 22:18:20 +0900

    Model 작성

---
+++ UnitConverter/Models/Number.cs
@@ -0,0 +1,25 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitConverter.Models
+{
+	internal class Number : ObservableObject
+	{
+		private double _inputNumber;
+		public double InputNumber
+		{
+			get
+			{
+				return _inputNumber;
+			}
+			set
+			{
+				SetProperty(ref _inputNumber, value);
+			}
+		}
+	}
+}
